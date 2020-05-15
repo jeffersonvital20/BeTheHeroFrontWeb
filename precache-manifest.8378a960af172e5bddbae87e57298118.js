2073e8026fffdf14b54bc1380764f81672c10d64
jeffersonvital20 <jeffersonvital20@gmail.com>
2020-05-14 22:07:27 -0300

    Updates

---
+++ precache-manifest.8378a960af172e5bddbae87e57298118.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "945052c0f73844b46e04fa91d4e50876",
+    "url": "/BeTheHeroFrontWeb/index.html"
+  },
+  {
+    "revision": "8cc1c098ea765431eb53",
+    "url": "/BeTheHeroFrontWeb/static/css/main.96708628.chunk.css"
+  },
+  {
+    "revision": "d0367aad9f699199c996",
+    "url": "/BeTheHeroFrontWeb/static/js/2.81569aa4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/BeTheHeroFrontWeb/static/js/2.81569aa4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cc1c098ea765431eb53",
+    "url": "/BeTheHeroFrontWeb/static/js/main.f773a473.chunk.js"
+  },
+  {
+    "revision": "9755b6072c9ea58f47cb",
+    "url": "/BeTheHeroFrontWeb/static/js/runtime-main.a804dc0d.js"
+  },
+  {
+    "revision": "1f4b5508873e87c04ab679e647a7954b",
+    "url": "/BeTheHeroFrontWeb/static/media/heroes.1f4b5508.png"
+  },
+  {
+    "revision": "7eea718b48893e461f82049ec46c1e1c",
+    "url": "/BeTheHeroFrontWeb/static/media/logo.7eea718b.svg"
+  }
+]);
